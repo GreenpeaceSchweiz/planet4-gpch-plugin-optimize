bd1699db934b916f0d3c9470552f99ddf6b06646
Stefan Dürrenberger <stefan.duerrenberger@greenpeace.org>
2025-02-19 16:35:17 +0100

    Style corrections for Planet4 compatibility

--- build/blocks/content-optimization/index.asset.php
+++ build/blocks/content-optimization/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n', 'wp-primitives'), 'version' => 'acc71f0bed410ae6528e');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n', 'wp-primitives'), 'version' => '2dbbfe60c1aad2da220b');
