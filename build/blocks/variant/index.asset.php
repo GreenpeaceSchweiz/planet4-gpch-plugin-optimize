864cd4374ef8ce44e164b37ed1c8674b9ceba2ea
Stefan Dürrenberger <stefan.duerrenberger@greenpeace.org>
2025-04-17 10:14:22 +0200

    Update version & build

--- build/blocks/variant/index.asset.php
+++ build/blocks/variant/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'cd97588f727329ddf70d');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '75eb29e17b1e1543cdb7');
