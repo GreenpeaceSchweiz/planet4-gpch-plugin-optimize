b335a3fe5747ab0b461af260c0ad08bb5ed9bc1a
Stefan Dürrenberger <sd@pm7.ch>
2026-06-09 16:25:17 +0200

    Update version & build

--- build/blocks/variant/index.asset.php
+++ build/blocks/variant/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'd6496c9a32d01ad15509');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'a0efbee877a269071c0c');
